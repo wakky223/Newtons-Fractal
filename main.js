f8bca981f871db91e88a7b2499de00cf1ad552d2
wakky223 <wakky223@outlook.com>
2021-11-16 19:25:15 -0330

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-class polynomial{
+/*class polynomial{
     constructor(zerosRe,zerosIm) {
         if(zerosRe.length != zerosIm.length){
             throw 'Coordinate Pair Exception.';
@@ -26,14 +26,59 @@ class polynomial{
           }
       return k;
     }
-  }
-  function multiplyPoly(p1,p2){
+  }*/
+
+//multiplies two polynoials, inputs are arrays starting at the x^0 term. 
+function multiplyPoly(p1,p2){
     let r = new Array(p1.length + p2.length - 1).fill(0);
-    for(i = 0; i < p1.length;i++){
+    for(let i = 0; i < p1.length;i++){
         for(j = 0; j < p2.length; j++){
             r[i+j] += p1[i] * p2[j];
         }
     }
+    return r;
+}
+
+//calculates the polynomial that has given complex conjugate zeros
+function imaginaryPolynomialZeros(realPart, imaginaryPart) {
+    r = [0, 0, 1];
+    r[0] = (realPart * realPart) + (imaginaryPart * imaginaryPart);
+    r[1] = -2 * realPart;
+    console.log("x^2 + " + r[1] + "x + " + r[0]);
+    return r;
+}
+
+//creates a polynomial based on real and imaginary zeros; assumes each complex zero has a conjugate
+function createPolynomial(zeros,czerosRP,czerosIP) {
+    if (czerosRP.length != czerosIP.length) {
+        throw 'Coordinate Pair Exception.';
+    }
+    let r = new Array(zeros.length + czerosRP.length + czerosIP + 1).fill(0);
+    console.log(r);
+    if (zeros.length > 0) {
+        r[0] = -zeros[0];
+        r[1] = 1;
+        console.log(r);
+        for (let i = 1; i < zeros.length; i++) {
+            r = multiplyPoly(r,[-zeros[i], 1]);
+        }
+        console.log(r);
+        for (let i = 0; i < czerosRP.length; i++) {
+            r = multiplyPoly(r, imaginaryPolynomialZeros(czerosRP[i], czerosIP[i]));
+        }
+        console.log(r);
+    } else if (czerosRp.length > 0) {
+        r[0] = imaginaryPolynomialZeros(czerosRP[0], czerosIP[0])[0];
+        r[1] = imaginaryPolynomialZeros(czerosRP[0], czerosIP[0])[1];
+        r[2] = 1;
+        for (i = 0; i < zeros.length; i++) {
+            r = multiplyPoly(r, [-zeros[i], 1]);
+        }
+        for (i = 1; i < czerosRP.length; i++) {
+            r = multiplyPoly(r, imaginaryPolynomialZeros(czerosRP[i], czerosIP[i]));
+        }
+    }
+    console.log(r[0] + ' + ' + r[1] + 'x + ' + r[2] + 'x^2 + ' + r[3] + 'x^3 + ' + r[4] + 'x^4 + ' + r[5] + 'x^5 + ' + r[6] + 'x^6 + ' + r[7] + 'x^7 + ')
     return r;
 }
 
