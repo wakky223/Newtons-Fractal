6fb566285d0db7564036603d6f9b330c15e0dc98
Landyn Heath <wakky223@outlook.com>
2021-12-10 02:10:51 -0330

    Preformance Optimizations & improved PolyToString

--- main.js
+++ main.js
@@ -15,7 +15,6 @@ function imaginaryPolynomialZeros(realPart, imaginaryPart) {
         let r = [0, 0, 1];
         r[0] = (realPart * realPart) + (imaginaryPart * imaginaryPart);
         r[1] = -2 * realPart;
-        console.log("x^2 + " + r[1] + "x + " + r[0]);
         return r;
     }else{
         return [-realPart,1];
@@ -29,7 +28,6 @@ function createPolynomial(zeros,czerosRP,czerosIP) {
         throw 'Coordinate Pair Exception.';
     }
     let r = new Array(zeros.length + czerosRP.length + czerosIP + 1).fill(0);
-    console.log(r);
     if (zeros.length > 0) {
         r[0] = -zeros[0];
         r[1] = 1;
@@ -51,7 +49,11 @@ function createPolynomial(zeros,czerosRP,czerosIP) {
             r = multiplyPoly(r, imaginaryPolynomialZeros(czerosRP[i], czerosIP[i]));
         }
     }
-    polyToString(r);
+    for(let i = 0; i < r.length;i++){
+        if( ((r[i] % 1) * (r[i] % 1)) % 1 < 0.000001 || ((r[i] % 1) * (r[i] % 1)) > 1 - 0.000001){
+            r[i] = math.round(r[i]);
+        }
+    }
     return r;
 }
 
@@ -61,7 +63,6 @@ function derivitive(p){
     for (let i = 0; i < p.length-1; i++) {
         r[i] = p[i+1]*(i+1);
     }
-    polyToString(r);
     return r;
 }
 
@@ -76,30 +77,38 @@ function eval(p, input) {
 
 //does one iteration of newtons method
 function iterate (v,p,d){
-    let r = eval(p,v).neg();
+    let r = eval(p,v);
+    r.re = -r.re;
+    r.im = -r.im;
     r = math.divide(r,eval(d,v));
-    r = math.add(r,v);
+    r.re += v.re;
+    r.im += v.im;
     return r; 
 }
 
-//iterates newtons method
-function newtonsMethod (v,p,iterations){
-    d = derivitive(p);
-    for(let i = 0;i < iterations;i++ ){
-        v = iterate(v,p,d);
-    }
-    return v;
-}
 
-function newtonsMethod (v,p,iterations,d){
+//iterates newtons method
+function newtonsMethod (v,p,d,rZero,iZero){
+    iterations = maxiterations/2
     for(let i = 0;i < iterations;i++ ){
         v = iterate(v,p,d);
+        v = iterate(v,p,d);
+        let distance = new Array(); 
+        for(let i = 0; i < rZero.length; i++){
+            distance[i] = math.sqrt( ((rZero[i]-v.re) * (rZero[i]-v.re)) + (iZero[i]-v.im) * (iZero[i]-v.im));
+        }
+        for(let i = 0; i < rZero.length; i++){
+            distance[i + rZero.length] = math.sqrt( ((rZero[i]-v.re) * (rZero[i]-v.re)) + (-iZero[i]-v.im) * (-iZero[i]-v.im));
+        } 
+        if(math.min(distance) < 0.01){
+            i = iterations;
+        }
     }
     return v;
 }
 
 function nearRoot(v,rZero,iZero){
-    let distance = new Array(rZero.length).fill(0); 
+    let distance = new Array(); 
     for(let i = 0; i < rZero.length; i++){
         distance[i] = math.sqrt( ((rZero[i]-v.re) * (rZero[i]-v.re)) + (iZero[i]-v.im) * (iZero[i]-v.im));
     }
@@ -111,48 +120,126 @@ function nearRoot(v,rZero,iZero){
 }
 
 //sets the screen
-function setScreen(iterations,rZero,iZero){
+function setScreen(rZero,iZero){
     console.time('drawFractal');
     p = createPolynomial([],rZero,iZero);
+    document.getElementById("div").innerHTML = polyToString(p);
+    polyToString(p);
     var id = ctx.createImageData(c.width,c.height);
-    d = derivitive(p);
+    var d = derivitive(p);
+    iterations = maxiterations/2
     var pixels = id.data;
     for(let x = 0; x < c.width; x++){
         for(let y = 0; y < c.height; y++){
             iterant = math.complex(scale * (x - h), -scale * (y - k));
-            iterant = newtonsMethod (iterant,p,iterations,d);
-            //find closest root
-            var g =  nearRoot(iterant,rZero,iZero) * 63.75;
-            var r = 100;
-            var b = 100;
+
+            //iterate newtons method
+            let distance = new Array(); 
+            for(let i = 0;i < iterations;i++ ){
+                iterant = iterate(iterant,p,d);
+                iterant = iterate(iterant,p,d);
+                for(let i = 0; i < rZero.length; i++){
+                    distance[i] = math.sqrt( ((rZero[i]-iterant.re) * (rZero[i]-iterant.re)) + (iZero[i]-iterant.im) * (iZero[i]-iterant.im));
+                }
+                for(let i = 0; i < rZero.length; i++){
+                    distance[i + rZero.length] = math.sqrt( ((rZero[i]-iterant.re) * (rZero[i]-iterant.re)) + (-iZero[i]-iterant.im) * (-iZero[i]-iterant.im));
+                } 
+                if(math.min(distance) < 0.001){
+                    var shading = i;
+                    i = iterations;
+                }
+            }
+            //set the color for the pixel
+            const closestRoot = math.min(distance);
+            if(shading != iterations/2-1 ){
+                var g =  distance.indexOf(closestRoot) * 63.75;
+                var r = 80 + shading * 10;
+                var b = 80 + shading * 10;
+            }else{
+                var r = 0;
+                var g = 0;
+                var b = 0;
+            }
+
             var offset = (y * id.width + x) * 4;
             pixels[offset] = r;
             pixels[offset + 1] = g;
             pixels[offset + 2] = b;
             pixels[offset + 3] = 255;
         }
+        console.log(((x/c.width) * 100 ));
     }
     ctx.putImageData(id, 0, 0);
     console.timeEnd('drawFractal');
 }
 
-//for dev purposes, converts a polynomial to a string to compare in other software
+//converts a polynomial to a string
 function polyToString(p){
-    let str = "";
-    for(let i = p.length-1; i > 0; i--){
-        str += p[i] + "x^" + i + " + ";
+    let str = ""
+    if(p[p.length-1] == 1){
+        str = "x<sup>" + (p.length-1) + "</sup>"; 
+    }else if(p[p.length-1] == -1){
+        str = "- x<sup>" + (p.length-1) + "</sup>"; 
+    }else{
+    str = p[p.length-1] + "x<sup>" + (p.length-1) + "</sup>";
     }
-    str += p[0];
-    console.log(str);
+    for(let i = p.length-2; i > 1; i--){
+        if(p[i] == 1){
+            str += " + x<sup>" + i + "</sup>"; 
+        }else if(p[i] == -1){
+            str += " - x<sup>" + i + "</sup>"; 
+        }else if(p[i] > 0){
+            str += " + " + p[i] + "x<sup>" + i + "</sup>";
+        } else if(p[i] < 0){
+            str += " - " + -p[i] + "x<sup>" + i + "</sup>";
+        }
+    }
+    if(p[1] == 1){
+        str += " + x"; 
+    }else if(p[1] == -1){
+        str += " - x"; 
+    }else if(p[1] > 0){
+        str += " + " + p[1] + "x" ;
+    } else if(p[1] < 0){
+        str += " - " + -p[1] + "x";
+    }
+    
+    if(p[0] > 0){
+        str += " + " + p[0];
+    } else if(p[0] < 0){
+        str += " - " + -p[0];
+    }
     return str;
 }
 
-//listen for enter keypress
+//listen for keypresses
 window.onkeypress = function(event) {
-    if (event.keyCode == 49) {
-        console.time('doSomething');
-        setScreen(1,[0,50,100],[0,200,500]);
-        console.timeEnd('doSomething');
+    if (event.keyCode == 48) {
+        scale *= 0.5;
+        console.log(scale);
+    } else if(event.keyCode == 57){
+        scale *= 2;
+        console.log(scale);
+    }else if(event.keyCode == 49){
+        console.log("Scale: " + scale);
+        console.log("Drawing: ");
+        setScreen([1,-0.5],[0,0.8660254037844386467637232]);
+    }else if(event.keyCode == 50){
+        console.log("Scale: " + scale);
+        console.log("Drawing: ");
+        setScreen([-1.769292354238631415240409,0.8846461771193157076202047],[0,0.5897428050222055016472807]);
+    }else if(event.keyCode == 51){
+        console.log("Scale: " + scale);
+        console.log("Drawing: ");
+        setScreen([1,0],[0,1]);
+    }else if(event.keyCode == 52){
+        console.log("Scale: " + scale);
+        console.log("Drawing: ");
+        setScreen([-1.769292354238631415240409,0.8846461771193157076202047],[0,0.5897428050222055016472807]);
+    }else if(event.keyCode == 53){
+        console.log("Scale: " + scale);
+        console.log("Drawing: ");
+        setScreen([-1.769292354238631415240409,0.8846461771193157076202047],[0,0.5897428050222055016472807]);
     }
 }   
 
@@ -166,6 +253,4 @@ c.height = window.innerHeight;
 var scale = 0.005;
 var h = c.width/2;
 var k = c.height/2;
-
-
-setScreen(20,[1,-0.5],[0,0.8660254037844386467637232]);
+var maxiterations = 200;
