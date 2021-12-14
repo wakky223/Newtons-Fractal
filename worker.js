4c0fc2f27035280a5699bc3194d18aa20ac43c2d
Landyn Heath <wakky223@outlook.com>
2021-12-14 01:24:58 -0330

    1

--- worker.js
+++ worker.js
@@ -115,10 +115,10 @@ function derivitive(p){
 function eval(p, input) {
     let r = math.complex(p[0],0);
     let x = input;
-    r = addComplex(r,multiplyComplex(p[1] , input));
+    r = addComplex(r,math.multiply(p[1] , input));
         for (let i = 2; i < p.length; i++) {
-            x = multiplyComplex(x,input)
-            r = addComplex(r,multiplyComplex(p[i] , x));
+            x = math.multiply(x,input)
+            r = addComplex(r,math.multiply(p[i] , x));
         }
     return r;
 }
